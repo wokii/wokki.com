900b66b78713c99beda90a4547397c1a03502d4c
wokki <hanwokki@gmail.com>
2026-01-18 21:25:47 +0000

    Fine Tuning2'

---
+++ src/app/components/sections/heroContent.ts
@@ -0,0 +1,5 @@
+export const heroTitles = [
+  { title: "Theory Engineer", note: "(philosopher: a.e.)" },
+  { title: "Systems Thinker", note: "(doer: e.m.)" },
+  { title: "aesthetic explorer", note: "(visionary: s.j.)" },
+];
