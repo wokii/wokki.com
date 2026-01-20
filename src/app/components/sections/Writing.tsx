3f04c1f4fe1f167d35f4737fd4cb0d907d912e59
wokki <hanwokki@gmail.com>
2026-01-20 07:34:02 +0000

    Quote(s)

--- src/app/components/sections/Writing.tsx
+++ src/app/components/sections/Writing.tsx
@@ -5,7 +5,33 @@ import SectionTitle from "./SectionTitle";
 export default function Writing() {
   return (
     <Section id="writing" minHeight="screen" paddingY="none">
-      <SectionTitle>WRITING</SectionTitle>
+      <SectionTitle
+        subtitle={
+          <span className="block max-w-[48ch] text-left">
+            <span
+              className="block"
+              style={{
+                paddingLeft: "1ch",
+                textIndent: "-1ch",
+              }}
+            >
+              &quot;Fugayzi, fugazi. It&apos;s a whazy. It&apos;s a woozie.
+              It&apos;s fairy dust. It doesn&apos;t exist. It&apos;s never
+              landed. It is no matter. It&apos;s not on the elemental chart.
+              It&apos;s not fucking real.&quot;
+            </span>
+            <span className="block text-right">
+              —{" "}
+              <span className="invert-selection inline-block bg-accent text-background px-1">
+                Mark H.
+              </span>{" "}
+              (The Wolf of Wall Street)
+            </span>
+          </span>
+        }
+      >
+        WRITING
+      </SectionTitle>
       <article>
         <h3 className="text-3xl">Nothing here yet.</h3>
         <p>Aug 24, 2025</p>
